672638318c59fc7c535315a0359c3aadc0e108f4
Vivek Kumar Biswal <vivek@Viveks-MacBook-Pro.local>
2023-11-16 21:05:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { WeatherApp } from "./Components/Weather/WeatherApp";
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp />
+    </div>
+  );
+}
+
+export default App;
